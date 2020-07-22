46da3dedb71c0b0e00efce573f3e178b226729b7
IhebChatti <iheb.chatti@holbertonschool.com>
2020-07-22 14:13:08 +0100

    is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t lheight, rheight;
 
-	if (!tree || (!(tree->left) && !(tree->right)))
+	if (!tree)
 		return (0);
 	lheight = binary_tree_height(tree->left) + 1;
 	rheight = binary_tree_height(tree->right) + 1;
